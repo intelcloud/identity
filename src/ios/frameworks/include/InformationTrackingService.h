3305f85e35c66cae74016aac2a248b726c50c95f
waldemar <waldemar.krumrick@darwoft.com>
2013-11-01 17:33:34 -0300

    Updated identity plugin iOS

---
+++ src/ios/frameworks/include/InformationTrackingService.h
@@ -0,0 +1,15 @@
+//
+//  InformationTrackingService.h
+//  ContextSDK
+//
+//  Created by Waldemar Krumrick on 9/10/13.
+//  Copyright (c) 2013 Intel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "StateProviderDelegate.h"
+#import <MapKit/MapKit.h>
+
+@interface InformationTrackingService : NSObject <UIApplicationDelegate, StateProviderDelegate>
+
+@end
